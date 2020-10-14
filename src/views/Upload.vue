6180663119eef7e0e380e46e4b66098c30b53833
wangchenyi1996 <2845189781@qq.com>
2020-10-14 16:23:43 +0800

    Signed-off-by: wangchenyi1996 <2845189781@qq.com>

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,231 +1,125 @@
 <template>
-  <div class="up-box">
-    <p class="up-txt">Node + H5 实现大文件分片上传、断点续传</p>
-    <!-- <div class="container">
-      <div class="row">
-        <div class="col-md-8">
-          <div class="wrap btn btn-default">
-            <input type="file" id="file" @change="fileChange" />
-          </div>
+  <div id="example">
+    <!-- 利用v-if…v-else切换 展开 和 收起 两个画面，template包裹多个元素 -->
+    <template v-if="isHide">
+      <!-- 只显示摘要的画面 -->
+      <div class="hideBg">
+        <p class="summary">{{ content }}</p>
+        <div class="showBtn">
+          <!-- 绑定点击事件onShow，点击展开全文 -->
+          <a href="#" @click.stop.prevent="onShow"
+            >展开阅读全文&nbsp;
+            <!-- 向下的角箭头符号，用css画 -->
+            <span class="downArrow"></span>
+          </a>
         </div>
       </div>
-      <div class="row" id="process1" :style="{display: dis1}">
-        <div class="col-md-4">校验文件进度</div>
-        <div class="col-md-8">
-          <div class="progress">
-            <div id="checkProcessStyle" class="progress-bar" :style="{width:w1}"></div>
-            <p id="checkProcessValue" class="value">{{percent1}}%</p>
-          </div>
+    </template>
+    <template v-else>
+      <!-- 显示完整内容的画面 -->
+      <div class="showBg">
+        <p>{{ content }}</p>
+        <div class="hideBtn">
+          <!-- 绑定点击事件onHide，点击收起内容 -->
+          <a href="#" @click.stop.prevent="onHide"
+            >收起&nbsp;
+            <!-- 向上的角箭头符号 -->
+            <span class="upArrow"></span>
+          </a>
         </div>
       </div>
-      <div class="row" id="process2" :style="{display: dis2}">
-        <div class="col-md-4">上传文件进度</div>
-        <div class="col-md-8">
-          <div class="progress">
-            <div id="uploadProcessStyle" class="progress-bar" :style="{width:w2}"></div>
-            <p id="uploadProcessValue" class="value">{{percent2}}%</p>
-          </div>
-        </div>
-      </div>
-    </div> -->
+    </template>
   </div>
 </template>
 
 <script>
-let baseUrl = "http://localhost:5000";
-let chunkSize = 5 * 1024 * 1024;
-let fileSize = 0;
-let file = null;
-let hasUploaded = 0;
-let chunks = 0;
-import SparkMD5 from "../lib/spark-md5.min.js";
-import axios from "axios";
 export default {
   data() {
     return {
-      percent1: 0,
-      percent2: 0,
-      dis1: false,
-      dis2: false,
-      w1: 0,
-      w2: 0,
+      content:
+        "中华人民共和国位于亚洲东部，太平洋西岸 [1]  ，是工人阶级领导的、以工农联盟为基础的人民民主专政的社会主义国家 [2]  。1949年（己丑年）10月1日成立 [3-4]  ，以五星红旗为国旗 [5]  ，《义勇军进行曲》为国歌 [6]  ，国徽内容包括国旗、天安门、齿轮和麦稻穗 [7]  ，首都北京 [8]  ，省级行政区划为23个省、5个自治区、4个直辖市、2个特别行政区 [9]  ，是一个以汉族为主体民族，由56个民族构成的统一多民族国家，汉族占总人口的91.51% [10]  。新中国成立后，随即开展经济恢复与建设 [11]  ，1953年开始三大改造 [12]  ，到1956年确立了社会主义制度，进入社会主义探索阶段 [13]  。文化大革命之后开始改革开放，逐步确立了中国特色社会主义制度。 [14] 中华人民共和国陆地面积约960万平方公里，大陆海岸线1.8万多千米，岛屿岸线1.4万多千米，内海和边海的水域面积约470多万平方千米。海域分布有大小岛屿7600多个，其中台湾岛最大，面积35798平方千米。 [1]  陆地同14国接壤，与6国海上相邻。",
+      isHide: true, //初始值为true，显示为折叠画面
     };
   },
   methods: {
-    // file 原生事件
-    fileChange(e) {
-      let file = e.target.files[0];
-      fileSize = file.size;
-      // console.log('文件信息',file)
-      this.responseChange(file);
+    onShow: function () {
+      this.isHide = false; //点击onShow切换为false，显示为展开画面
     },
-    // 0.响应点击
-    async responseChange(file) {
-      // 第一步：按照 修改时间+文件名称+最后修改时间-->MD5
-      // 显示文件校验进度
-      this.dsi1 = true;
-      // 开始校验
-      let fileMd5Value = await this.md5File(file);
-      // 第二步：校验文件的MD5
-      let result = await checkFileMD5(file.name, fileMd5Value);
-      // // 如果文件已存在, 就秒传
-      // if (result.file) {
-      //     alert('文件已秒传')
-      //     return
-      // }
-      // // 显示文件上传进度
-      //  this.dsi2 = true
-      // // 第三步：检查并上传MD5
-      // await checkAndUploadChunk(fileMd5Value, result.chunkList)
-      // // 第四步: 通知服务器所有分片已上传完成
-      // notifyServer(fileMd5Value)
-    },
-    // 1.修改时间+文件名称+最后修改时间-->MD5
-    md5File(file) {
-      let that = this;
-      return new Promise((resolve, reject) => {
-        var blobSlice =
-            File.prototype.slice ||
-            File.prototype.mozSlice ||
-            File.prototype.webkitSlice,
-          //chunkSize = 2097152, // Read in chunks of 2MB
-          chunkSize = file.size / 100,
-          //chunks = Math.ceil(file.size / chunkSize),
-          chunks = 100,
-          currentChunk = 0,
-          spark = new SparkMD5.ArrayBuffer(),
-          fileReader = new FileReader();
-
-        fileReader.onload = function (e) {
-          console.log("read chunk nr", currentChunk + 1, "of", chunks);
-          spark.append(e.target.result); // Append array buffer
-          currentChunk++;
-
-          if (currentChunk < chunks) {
-            loadNext();
-          } else {
-            let cur = +new Date();
-            console.log("finished loading");
-            // alert(spark.end() + '---' + (cur - pre)); // Compute hash
-            let result = spark.end();
-            resolve(result);
-          }
-        };
-
-        fileReader.onerror = function () {
-          console.warn("oops, something went wrong.");
-        };
-
-        function loadNext() {
-          var start = currentChunk * chunkSize,
-            end =
-              start + chunkSize >= file.size ? file.size : start + chunkSize;
-
-          fileReader.readAsArrayBuffer(blobSlice.call(file, start, end));
-          that.w1 = currentChunk + 1 + "%";
-          that.percent1 = currentChunk + 1;
-          that.w2 = currentChunk + 1 + "%";
-          that.percent2 = currentChunk + 1;
-        }
-
-        loadNext();
-      });
-    },
-    // 2.校验文件的MD5
-    checkFileMD5(fileName, fileMd5Value) {
-      return new Promise((resolve, reject) => {
-        let url =
-          baseUrl +
-          "/check/file?fileName=" +
-          fileName +
-          "&fileMd5Value=" +
-          fileMd5Value;
-        axios
-          .post(url)
-          .then(function (response) {
-            resolve(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      });
-    },
-    // 3.上传chunk
-    async checkAndUploadChunk(fileMd5Value, chunkList) {
-      chunks = Math.ceil(fileSize / chunkSize);
-      hasUploaded = chunkList.length;
-      for (let i = 0; i < chunks; i++) {
-        let exit = chunkList.indexOf(i + "") > -1;
-        // 如果已经存在, 则不用再上传当前块
-        if (!exit) {
-          let index = await upload(i, fileMd5Value, chunks);
-          hasUploaded++;
-          let radio = Math.floor((hasUploaded / chunks) * 100);
-          $("#uploadProcessStyle").css({
-            width: radio + "%",
-          });
-          $("#uploadProcessValue").html(radio + "%");
-        }
-      }
-    },
-
-    // 3-2. 上传chunk
-    upload(i, fileMd5Value, chunks) {
-      return new Promise((resolve, reject) => {
-        //构造一个表单，FormData是HTML5新增的
-        let end =
-          (i + 1) * chunkSize >= file.size ? file.size : (i + 1) * chunkSize;
-        let form = new FormData();
-        form.append("data", file.slice(i * chunkSize, end)); //file对象的slice方法用于切出文件的一部分
-        form.append("total", chunks); //总片数
-        form.append("index", i); //当前是第几片
-        form.append("fileMd5Value", fileMd5Value);
-        $.ajax({
-          url: baseUrl + "/upload",
-          type: "POST",
-          data: form, //刚刚构建的form数据对象
-          async: true, //异步
-          processData: false, //很重要，告诉jquery不要对form进行处理
-          contentType: false, //很重要，指定为false才能形成正确的Content-Type
-          success: function (data) {
-            resolve(data.desc);
-          },
-        });
-      });
-    },
-
-    // 第四步: 通知服务器所有分片已上传完成
-    notifyServer(fileMd5Value) {
-      let url =
-        baseUrl +
-        "/merge?md5=" +
-        fileMd5Value +
-        "&fileName=" +
-        file.name +
-        "&size=" +
-        file.size;
-      $.getJSON(url, function (data) {
-        alert("上传成功");
-      });
-    },
-
-    getDate() {
-      let d = new Date();
-      return d.getMinutes() + ":" + d.getSeconds() + " " + d.getMilliseconds();
+    onHide: function () {
+      this.isHide = true; //点击onHide切换为true，显示为折叠画面
     },
   },
 };
 </script>
 
 <style lang="scss" scoped>
-.up-box {
-  height: 100vh;
-  width: 100vw;
-  .up-txt {
-    font-size: 16px;
-    font-weight: bold;
-    color: orangered;
-    margin: 20px;
-  }
+/* 摘要背景板 */
+.hideBg {
+  width: 500px;
+  background-color: #e9ecef;
+  margin: 1.5rem;
+  padding: 1.5rem;
+  padding-bottom: 0; /* 方便渐变层遮挡 */
+  position: relative; /* 用于子元素定位 */
+}
+/* 全文背景板，基本与摘要相同 */
+.showBg {
+  width: 500px;
+  background-color: #e9ecef;
+  margin: 1.5rem;
+  padding: 1.5rem;
+}
+/* 摘要内容 */
+.summary {
+  overflow: hidden; /* 隐藏溢出内容 */
+  text-overflow: clip; /* 修剪文本 */
+  display: -webkit-box; /* 弹性布局 */
+  -webkit-box-orient: vertical; /* 从上向下垂直排列子元素 */
+  -webkit-line-clamp: 3; /* 限制文本仅显示前三行 */
+}
+#example p {
+  text-indent: 2em;
+}
+/* 展开按钮 */
+.showBtn {
+  width: 100%; /* 与背景宽度一致 */
+  height: 3rem;
+  position: absolute; /* 相对父元素定位 */
+  top: 3rem; /* 刚好遮挡在最后两行 */
+  left: 0;
+  z-index: 0; /* 正序堆叠，覆盖在p元素上方 */
+  text-align: center;
+  background: linear-gradient(
+    rgba(233, 236, 239, 0.5),
+    white
+  ); /* 背景色半透明到白色的渐变层 */
+  padding-top: 3rem;
+}
+/* 收起按钮 */
+.hideBtn {
+  text-align: right;
+}
+#example a {
+  text-decoration: none; /* 清除链接默认的下划线 */
+}
+/* 向下角箭头 */
+.downArrow {
+  display: inline-block;
+  width: 8px; /* 尺寸不超过字号的一半为宜 */
+  height: 8px;
+  border-right: 1px solid; /* 画两条相邻边框 */
+  border-bottom: 1px solid;
+  transform: rotate(45deg); /* 顺时针旋转45° */
+  margin-bottom: 3px;
+}
+/* 向上角箭头，原理与下箭头相同 */
+.upArrow {
+  display: inline-block;
+  width: 8px;
+  height: 8px;
+  border-left: 1px solid;
+  border-top: 1px solid;
+  transform: rotate(45deg);
+  margin-top: 3px;
 }
 </style>>
